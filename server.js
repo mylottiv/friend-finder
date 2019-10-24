92b82b364286a506620a0649d4644c4bea7be2db
Thomas Mylott <mylottiv@gmail.com>
2019-10-24 16:52:50 -0500

    Basic server implementation, successfully imports and implements routes from route modules

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 // Init node modules
 const express = require('express');
 const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Init routing modules
+const apiRouter = require('./app/routing/apiRoutes.js');
+const htmlRouter = require('./app/routing/htmlRoutes.js');
+app.use(apiRouter);
+app.use(htmlRouter);
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Launch server
+app.listen(PORT, function () {
+    console.log('Listening on port', PORT);
+})
