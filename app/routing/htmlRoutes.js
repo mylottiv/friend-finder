e2cadeba905eb70ac04f88bf53fe77d13cbbab29
Thomas Mylott <mylottiv@gmail.com>
2019-10-29 15:48:03 -0500

    Actually fixed routing bugs including a 404 default case and falling through to the /survey route

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,10 +4,20 @@ const path = require('path');
 const app = module.exports = express();
 
 // Set HTML Controllers
-app.get('/:home?', function(req, res){
-    res.sendFile(path.join(__dirname.replace('routing', 'public'),'home.html'));
+app.get('/:home?', function(req, res, next){
+    if (!req.params.home || req.params.home === 'home') {
+        console.log(req.params.home, 'triggered')
+        res.sendFile(path.join(__dirname.replace('routing', 'public'),'home.html'));
+    }
+    else {
+        next('route');
+    }
 })
 
 app.get('/survey', function(req, res){
     res.sendFile(path.join(__dirname.replace('routing', 'public'),'survey.html'));
 })
+
+app.use(function(req, res){
+    res.sendStatus(404);
+});
